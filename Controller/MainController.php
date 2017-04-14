7463860baf2a08f3b8b99627b6542d77c1af50a9
Kwon <youngbin.kwon@rakuten.com>
2017-04-14 21:42:22 +0900

    aa

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -18,6 +18,8 @@ class MainController
     {
         $customer = new Customer();
         $this->view->customer = $customer->selectAll();
+        $tts = new TextToSpeech();
+        $this->view->result_text = $tts->getAudio("予約したいゴルフ場を教えてね");
         $this->view->display("Main/index.tpl");
     }
 }

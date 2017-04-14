6df6e1f94486fb7348a314778e81ee66ea7de2d7
Convolbo <moise.convolbo@rakuten.com>
2017-04-14 14:31:49 +0900

    changes

--- Controller/TradeOffController.php
+++ Controller/TradeOffController.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(E_ALL);
 
 /**
  * Created by PhpStorm.
@@ -17,53 +18,149 @@ class TradeOffController
     public function __construct($url)
     {
 
+
+        $string = file_get_contents("problem_w.json");
         $this->model = new TradeOffModel();
         $this->view = new Template();
-        $this->url = $url;
-        $this->dilemmaFile =  array('data' => 'problem.json');
+        $this->url = "https://gateway.watsonplatform.net/tradeoff-analytics/api/v1/dilemmas/";
+        $this->dilemmaFile = array('data' =>"@problem_w.json");
+        $this->url_base = 'https://gateway.watsonplatform.net/tradeoff-analytics/api';
+        $this->username = TRADEOFFUSERNAME;
+        $this->password = TRADEOFFPASSWORD;
+    }
+
+    public function UserTradeoffAPI(){
+        $endpoint_url = "https://gateway.watsonplatform.net/tradeoff-analytics/api/v1/dilemmas/?find_preferable_options=true";
+        $credentials = [
+            "username" => TRADEOFFUSERNAME,// コピペする
+            "password" => TRADEOFFPASSWORD// コピペする
+        ];
+        $credentials_text = "{$credentials['username']}: {$credentials['password']}";
+
+        /* ============OBJECTIVE INPUT=================== */
+        $rows[] = array(
+            "name" => "price",
+            "type" => "numeric",
+            "is_objective" => true,
+            "goal" => "min"
+        );
+        $rows[] = array(
+            "name" => "weather",
+            "type" => "numeric",
+            "is_objective" => true,
+            "goal" => "max"
+        );
+        $rows[] = array(
+            "name" => "distance",
+            "type" => "numeric",
+            "is_objective" => true,
+            "goal" => "min"
+        );
+        /*================================================= */
+
+        /* ============OBJECTIVE COLUMNS=================== */
+        $count=1;
+        foreach ($rows as $key => $value){
+            $objective_items[] = array(
+                "key" => "obj".$count++,
+                "full_name" => $value['name'],
+                "type" => $value['type'],
+                "is_objective" => $value['is_objective'],
+                "goal" => $value['goal']
+            );
+
+        }
+
+        /*================================================= */
+
+
+
+
+        $request = [
+            "subject" => "holiday",
+            "columns" => [
+                array("key" => "price",
+                    "full_name" => "Price",
+                    "type" => "numeric",
+                    "is_objective" => true,
+                    "goal" => "min"),
+
+                array("key" => "engineSize",
+                    "full_name" => "Engine Volume",
+                    "type" => "numeric",
+                    "is_objective" => false,
+                    "goal" => "min"),
+
+                array("key" => "power",
+                    "full_name" => "Power",
+                    "type" => "numeric",
+                    "is_objective" => true,
+                    "goal" => "max")
+            ],
+            "options" => [
+               array( "key" => 200727569,
+                        "name" => "Acura ILX",
+                        "description" =>"Technology Plus Package 4dr Sedan (2.4L 4cyl 8AM)",
+                        "values"=> array(
+                        "price" => 32900,
+                        "engineSize" => 3.4,
+                         "power" => 501)),
+
+                array( "key" => 200726801,
+                    "name" => "Acura MDX",
+                    "description" =>"4dr SUV (3.5L 6cyl 9A)",
+                    "values"=> array(
+                        "price" => 33015,
+                        "engineSize" => 3.5,
+                        "power" => 290))
+        ]
+    ];
+
+        $ch = curl_init($endpoint_url);
+        curl_setopt($ch, CURLOPT_USERPWD, $credentials_text);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($request));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+
+
+        $result = curl_exec($ch);
+        $err = curl_errno($ch);
+        $err_or = curl_error($ch);
+
+        curl_close($ch);
+
+
+
+        $ResultArray = json_decode($result);
+
+        $solution_Space = $ResultArray->resolution;
+        $solutions = $solution_Space->solutions;
+        foreach ($solutions as $key => $val){
+            $bestSolution[$key]["planId"] = $val->solution_ref;
+            $bestSolution[$key]["status"] = $val->status;
+        }
+
+
+        $PreferableSolution = $solution_Space->preferable_solutions;
+        $preferredList['list'] = $PreferableSolution->solution_refs;
+        $preferredList['score'] = $PreferableSolution->score;
+
+        $param =array();
+        $param['Preferable_List'] = $preferredList['list'];
+        $param['Confidence'] = $preferredList['score'];
+        $param['BestSolutionList'] = $bestSolution[$key]["planId"];
+        $param['BestSolutionStatus'] = $bestSolution[$key]["status"];
+
+        return $param;
     }
 
 
-        public function SolveDilemma(){
-
-
-            // create a new cURL resource
-            $ch = curl_init();
-            // set URL and other appropriate options
-            curl_setopt($ch, CURLOPT_URL, $this->url);
-            curl_setopt($ch, CURLOPT_HEADER, array('Content-Type: application/json','Accept: application/xml'));
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $this->dilemmaFile);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch, CURLOPT_POST, 0);
-            curl_setopt($ch, CURLOPT_SAFE_UPLOAD, true);
-            curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-            curl_setopt($ch, CURLOPT_USERPWD, TRADEOFFUSERNAME .":".TRADEOFFPASSWORD);
-            curl_setopt($ch, CURLOPT_VERBOSE, true);
-            curl_setopt($ch, CURLOPT_STDERR, fopen("headers.txt", "w+"));
-            $ckfile = tempnam ("/", 'MyCookie');
-            curl_setopt ($ch, CURLOPT_COOKIEFILE, $ckfile);
-
-            $result = curl_exec($ch);
-            $info = curl_getinfo($ch);
-
-            var_dump($result);
-
-            // Will dump a beauty json :3
-            //var_dump(json_decode($result, true));
-            $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-            $header = substr($result, 0, $header_size);
-            $body = substr($result, $header_size);
-            $result = json_decode($body, true);
-
-
-            $err = curl_getinfo($ch,CURLINFO_HTTP_CODE);
-            // close cURL resource, and free up system resources
-            curl_close($ch);
-
-            exit();
-
+        public function indexAction(){
+              $this->view->TradeOffResults = $this->UserTradeoffAPI();
+              $this->view->display('Tradeoff/index.tpl');
         }
 
 }

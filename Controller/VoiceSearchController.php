ffc9d6ea03355d0015b4c3a6eb5ef139cc6ced16
keisei.matsumoto <keisei.matsumoto@rakuten.com>
2017-04-17 20:51:32 +0900

    faaa

--- Controller/VoiceSearchController.php
+++ Controller/VoiceSearchController.php
@@ -15,106 +15,12 @@ class VoiceSearchController
 
     public function searchAction()
     {
-        if (isset($_POST["audio"])) {
-            $stt = new SpeechToText();
-            //Get text from voice
-            $voice_result = $stt->getText($_POST["audio"]);
-            $results = $voice_result->results;
-            if ($results[0]->alternatives) {
-                $alternatives = $results[0]->alternatives;
-                $transcript = $alternatives[0]->transcript;
-            } else {
-                echo('もう一度お願いします。');
-                $this->view->display("VoiceSearch/search.tpl");
-            }
-
-            $this->view->display("VoiceSearch/search.tpl");
-$this->view->display("VoiceSearch/search.tpl");
-
-$transcript = "福岡県朝からゴルフ来月 ";
-
-            //Get parameters from text
-            $nlc = new NaturalLanguageClassifier();
-            $divided_words = $nlc->divideWordsByIgo($transcript);
-            $class_results = $nlc->classifyWords($divided_words);
-
-            //Call Gora API and get plan
-            $gora_plan = new GoraPlanSearch();
-            $params = $gora_plan->setParam($class_results);
-            $gora_result = $gora_plan->getPlan($params);
-            $plan_info_array = [];
-            $course_id_array = [];
-            $destination = [];
-
-            $i = 0;
-
-            foreach ($gora_result['Items'] as $key => $item) {
-                if (!empty($item['Item']['planInfo'])) {
-                    foreach ($item['Item']['planInfo'] as $plan) {
-                            $plan_info_array[$plan['plan']['planId']] = $plan['plan'];
-                            $plan_info_array[$plan['plan']['planId']]['golfCourseId'] = $item['Item']['golfCourseId'];
-                            $course_id_array[$item['Item']['golfCourseId']] = $item['Item']['golfCourseId'];
-                            $destination[$item['Item']['golfCourseId']]['from'] = "東京";
-                            $course_name = preg_replace("/(\(|（).*(\)|）)/","",$item['Item']['golfCourseName']);
-                            $course_name = preg_replace("/【(.*?)】/","",$course_name);
-                            $destination[$item['Item']['golfCourseId']]['to'] = $course_name;
-                            $i++;
-                    }
-                }
-                if ($i == 5) {
-                    break;
-                }
-            }
-
-            //Make parameters for tradeoff
-            $google_map = new GoogleMapApi();
-            foreach ($course_id_array as $course_id) {
-                $course_info[$course_id]['time'] = $google_map->getDistance($destination[$course_id]['from'], $destination[$course_id]['to']);
-                $course_info[$course_id]['weather'] = 100;
-            }
-
-            $trade_off_data = [];
-            foreach ($plan_info_array as $plan_id => $plan_info) {
-                $trade_off_data[$plan_id]['price'] = $plan_info['price'];
-                
-                // TODO make function to get time
-                $trade_off_data[$plan_id]['distance'] = $course_info[$plan_info['golfCourseId']]['time'];
-                
-                // TODO make function to get weather
-                $trade_off_data[$plan_id]['weather'] = $course_info[$plan_info['golfCourseId']]['weather'];
-                
-                if (isset($plan_info['startTimeZone'])) {
-                    $time = preg_replace("/時台/","",$plan_info['startTimeZone']);
-                    $time = preg_replace("/、/",",",$time);
-                    $trade_off_data[$plan_id]['time'] = $time;
-                } else {
-                    $trade_off_data[$plan_id]['time'] = rand(5, 15);
-                }
-            }
-            //Run tradeoff
-            $recommend_plan_id = $plan_id;
-            /* 
-            TODO make tradeoff
-            $trade_off = new Tradeoff();
-            $recommend_plan_id = $trade_off->getRecommendPlan($trade_off_data);
-            */
-
-            //Get golf course detail to show
-            $gora_course = new GoraCourseDetail();
-            $course_info = $gora_course->getCOurseDetail($plan_info_array[$recommend_plan_id]['golfCourseId']);
-
-            //Make parameters for display
-            $display_data['plan'] = $plan_info_array[$plan_id];
-            $display_data['course'] = $course_info;
-            $this->view->class_results = $class_results;
-        }
-
         $this->view->display("VoiceSearch/search.tpl");
     }
 
     public function resultAction(){
         if (isset($_POST["audio"])) {
-
+/*
             $user_id   = $_POST["userid"];
             $customer  = new Customer();
             $user_info = $customer->selectById($user_id);
@@ -271,7 +177,8 @@ $transcript = "福岡県朝からゴルフ来月 ";
             $this->view->result_text = $tts->getAudio($course_info['Item']['golfCourseNameKana'] . "の" . $plan_info_array[$recommend_plan_id]['planName']  . " が おすすめだよ " . " 予約いたしましょうか？");
         }
 
-
+*/
+exit();
         $this->view->display("VoiceSearch/result.tpl");
     }
 }
